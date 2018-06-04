673eeb4d820eba2530c2ff02d3d953c0c2d02fb2
Ilham Wahabi <wahabiputra@gmail.com>
2018-06-05 04:30:47 +0700

    Able deploy to github

--- src/components/Tictactoe/Tictactoe.js
+++ src/components/Tictactoe/Tictactoe.js
@@ -6,7 +6,10 @@ export default class Tictactoe extends React.Component {
   render(){
     return (
       <section className="ttt">
-        <h1>Tic tac toe</h1>
+        <div>
+          <h1>Plutonic</h1> 
+          <h1>Tic-Tac-Toe</h1>
+        </div>
       </section>
     )
   }
