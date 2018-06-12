5d70727b45f1c64d06db0b89085e1dcbf147b54f
Ilham Wahabi <wahabiputra@gmail.com>
2018-06-12 16:31:17 +0700

    Initialize box

--- src/components/Tictactoe/Tictactoe.js
+++ src/components/Tictactoe/Tictactoe.js
@@ -6,10 +6,17 @@ export default class Tictactoe extends React.Component {
   render(){
     return (
       <section className="ttt">
-        <div>
-          <h1>Plutonic</h1> 
-          <h1>Tic-Tac-Toe</h1>
-        </div>
+        <div className="box-section">
+          <div className="box box1"><div className="mark"></div></div>
+          <div className="box box2"><div className="mark"></div></div>
+          <div className="box box3"><div className="mark"></div></div>
+          <div className="box box4"><div className="mark"></div></div>
+          <div className="box box5"><div className="mark"></div></div>
+          <div className="box box6"><div className="mark"></div></div>
+          <div className="box box7"><div className="mark"></div></div>
+          <div className="box box8"><div className="mark"></div></div>
+          <div className="box box9"><div className="mark"></div></div>
+        </div>        
       </section>
     )
   }
