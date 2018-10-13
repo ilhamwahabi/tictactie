cedd38f9c8daa304e0b10564fedeb9c6b72bc001
Ilham Wahabi <wahabiputra@gmail.com>
2018-10-13 23:59:57 +0700

    Add about swal

--- src/components/Interact.js
+++ src/components/Interact.js
@@ -1,15 +1,32 @@
 import React, { Component, Fragment } from 'react'
+import swal from 'sweetalert2'
 
 import './css/Interact.css'
 
 class Interact extends Component {
+  openAboutSwal = () => {
+    swal({
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/6/64/Tic-tac-toe.png',
+      imageWidth: 200,
+      imageHeight: 200,
+      title: 'Tic Tac Boom!',
+      text: 'Copyright 2018 Ilham Wahabi',
+      confirmButtonText: 'Cool',
+      footer: `
+        <a class="see-github" href="https://github.com/IlhamWahabiGX/tictac-boom">
+          See this project on &nbsp;<ion-icon name="logo-github"></ion-icon>
+        </a>
+      `
+    })
+  }
+
   render() {
     return (
       <Fragment>
         <div className="settings-button">
           <i className="material-icons md-48 button-icon">settings</i>
         </div>      
-        <div className="about-button">
+        <div className="about-button" onClick={this.openAboutSwal}>
           <i className="material-icons md-48 button-icon">face</i>
         </div>      
       </Fragment>
