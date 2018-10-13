46817c327017c76ac1cfff91f9af34a47c3f7f28
Ilham Wahabi <wahabiputra@gmail.com>
2018-10-13 23:38:30 +0700

    Add interact corner button

--- src/components/Interact.js
+++ src/components/Interact.js
@@ -6,7 +6,12 @@ class Interact extends Component {
   render() {
     return (
       <Fragment>
-        <div></div>      
+        <div className="settings-button">
+          <i className="material-icons md-48 button-icon">settings</i>
+        </div>      
+        <div className="about-button">
+          <i className="material-icons md-48 button-icon">face</i>
+        </div>      
       </Fragment>
     )
   }
