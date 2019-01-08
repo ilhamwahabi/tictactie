ef09b1e020a0526b15820fa53d404997cdc9894b
IlhamWahabiGX <wahabiputra@gmail.com>
2019-01-08 20:35:51 +0700

    Updates

--- precache-manifest.7332789eaf04a9814d91b0af14ae7f27.js
+++ precache-manifest.7332789eaf04a9814d91b0af14ae7f27.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/tictac-boom/static/js/runtime~main.229744d5.js"
   },
   {
-    "revision": "a28d187ed5a457896da1",
-    "url": "/tictac-boom/static/js/main.a28d187e.chunk.js"
+    "revision": "542825ed15a37da1e8a8",
+    "url": "/tictac-boom/static/js/main.542825ed.chunk.js"
   },
   {
     "revision": "73eb3620b927dc520515",
     "url": "/tictac-boom/static/js/1.73eb3620.chunk.js"
   },
   {
-    "revision": "a28d187ed5a457896da1",
+    "revision": "542825ed15a37da1e8a8",
     "url": "/tictac-boom/static/css/main.04bed974.chunk.css"
   },
   {
-    "revision": "18edebd5d09eb71d49cc7b281782fffb",
+    "revision": "299307d2b5bbfc8f14d8e19a99e3ab12",
     "url": "/tictac-boom/index.html"
   }
 ];
