95bb17369f45d063209e753fcdffe2fa3cf09593
Ilham Wahabi <wahabiputra@gmail.com>
2019-12-01 01:54:57 +0700

    remove sw

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import App from "./App";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById("root"));
